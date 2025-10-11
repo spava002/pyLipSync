0604934ddfc6c0baff381994ebf948b18205d9f7
Sebas P <spava002@fiu.edu>
2025-10-11 17:55:50 -0400

    Modified to include volume in output, but use a vector for silence phoneme calculation.

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import librosa as lb
 from pyLipSync import LipSync, CompareMethod
 
 lipsync = LipSync(
-    # compare_method=CompareMethod.COSINE_SIMILARITY # Can choose from L1Norm, L2Norm, or CosineSimilarity for comparison
-    silence_threshold=0.3
+    compare_method=CompareMethod.COSINE_SIMILARITY # Can choose from L1Norm, L2Norm, or CosineSimilarity for comparison
 )
 
 # Load in a test audio file to generate phoneme segments for
-# audio, sr = lb.load("pyLipSync/audio/oh/O_female.mp3", sr=None)
-audio, sr = lb.load("test_female.mp3", sr=None)
+audio, sr = lb.load("pyLipSync/audio/oh/O_female.mp3", sr=None)
 
 segments = lipsync.process_audio_segments(audio, sr)
 
