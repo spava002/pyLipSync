64c9aedad4d079753c6c0c0562c317a01f8dca43
Sebas P <spava002@fiu.edu>
2025-10-11 15:50:13 -0400

    Removed calculation of silence phoneme based on volume.

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ lipsync = LipSync(
 
 # Load in a test audio file to generate phoneme segments for
 # audio, sr = lb.load("pyLipSync/audio/oh/O_female.mp3", sr=None)
-audio, sr = lb.load("11_audio.wav", sr=None)
+audio, sr = lb.load("test_female.mp3", sr=None)
 
 segments = lipsync.process_audio_segments(audio, sr)
 
 # View the most prominent phoneme for each segment
 for segment in segments:
-    print((segment.get_max_target_phoneme(), segment.rms_volume))
+    print((segment.get_max_target_phoneme(), segment.volume, segment.normalized_volume))
