f355839862bbb5955b10b3d279d98b687ba6cd3e
Spava <spava002@fiu.edu>
2025-10-13 20:28:04 -0400

    Updated version.

--- pyLipSync/__init__.py
+++ pyLipSync/__init__.py
@@ -2,4 +2,5 @@ from .types import Phoneme, PhonemeSegment, LipSyncInfo
 from .comparison import CompareMethod
 from .core import LipSync
 
+__version__ = "0.1.0"
 __all__ = ["Phoneme", "PhonemeSegment", "LipSyncInfo", "CompareMethod", "LipSync"]
